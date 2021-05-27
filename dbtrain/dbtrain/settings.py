60a60f24fb3b63049d83d2c8a5e49af12edd2b2a
Samir_Lehaff <samir@lehaff.dk>
2021-05-27 10:10:34 +0200

    sa

--- dbtrain/dbtrain/settings.py
+++ dbtrain/dbtrain/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'u1wnet',
+    'u2knet',
 ]
 
 MIDDLEWARE = [
---
+++ dbtrain/u1wnet/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WnetConfig(AppConfig):
+    name = 'wnet'
