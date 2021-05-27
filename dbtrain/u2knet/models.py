3d9d6e311ed9c7db331103879b33bbde22ce363f
Samir_Lehaff <samir@lehaff.dk>
2021-05-27 15:16:48 +0200

    sa

--- dbtrain/u2knet/models.py
+++ dbtrain/u2knet/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
+# Create your models here.
+
+class Un2models(models.Model):
+
+    path=models.FilePathField(path ="/home/images",
+              match ="u1models.*", 
+              recursive = True)
+    name=models.CharField(max_length=128)
+    version=models.CharField(max_length=128)
+    trdata=models.FilePathField(path ="/home/images",
+              match ="train.*", 
+              recursive = True)
+    datecreated=models.DateTimeField(auto_created=True, blank=True)
+    dateupdated=models.DateTimeField(auto_now_add=True, blank=True)
+    hyperparams= models.TextField()
+
+
+class TrainingData(models.Model):
+
+    path=models.FilePathField(path ="/home/images",
+              match ="u2models.*", 
+              recursive = True)
+    name=models.CharField(max_length=128)
+    samples=models.IntegerField()
+    datecreated=models.DateField()
+
+
+class ValidationData(models.Model):
+    path=models.FilePathField(path ="/home/images",
+              match ="u2models.*", 
+              recursive = True)
+    name=models.CharField(max_length=128)
+    samples=models.IntegerField()
+    datecreated=models.DateField()
 # Create your models here.
